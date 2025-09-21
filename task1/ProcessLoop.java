21819c47a6aa88bb0541f49cdd9253c18ce6cdb2
Mohamed Taha <112260594+MahamedTaha@users.noreply.github.com>
2025-09-21 03:12:20 +0300

    Update ProcessLoop.java

--- task1/ProcessLoop.java
+++ task1/ProcessLoop.java
@@ -1,1 +1,26 @@
 
+public class task2 {
+    public static void main(String[] args) {
+
+        int x = 2;
+        int num = 4;
+        double sum = 0.0;
+        for (int i = 1; i <= num; i++) {
+            double res = 1;
+            for (int j = 0; j < i; j++) {
+                res *= x;
+            }
+            double res2 = 1;
+            for (int k = i; k >= i; k--) {
+                res2 *= k;
+            }
+
+            double div = res / res2;
+            sum += div;
+
+        }
+        System.out.println(sum);
+
+    }
+
+}
